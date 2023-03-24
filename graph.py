85627994dec7d44a3e4fc849f62e79d723c99ea3
sohaboumaima <ous219@lehigh.edu>
2023-03-23 22:10:32 -0400

    Made graph.py with the essential graph operations

---
+++ graph.py
@@ -0,0 +1,49 @@
+import pygame
+class Graph:
+
+    # Initialize the graph.
+    def __init__(self):
+        self.g = {}
+        pass
+
+    # Return the list of nodes in the graph.
+    def get_node_list(self):
+        # To be implemented
+        pass
+
+    # Return the list of neighbors of a node.
+    def get_neighbors(self, node):
+        # To be implemented
+        pass
+
+    # Return list of edges in the form (i, j).
+    def get_edge_list(self):
+        # To be implemented
+        pass
+
+    # Add a new node to the graph.
+    def add_node(self, v):
+        # To be implemented
+        pass
+
+    # Delete edge
+    def del_edge(self, v, w):
+        pass
+
+    # Delete node from the graph
+    def del_node(self, v):
+        for k in self.g[v]:
+            self.g[k].remove[v]
+        del self.g[v]
+        pass
+
+    # Add edge connecting the nodes v and w
+    def add_edge(self, v, w):
+        self.g[v].append(w)
+        self.g[w].append(v)
+        pass
+
+    # Returns the path from node u to node v, if it exists. Return empty list otherwise.
+    def find_path(self, u, v):
+        pass
+
